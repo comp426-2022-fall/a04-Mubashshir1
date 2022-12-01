981f55273ed9f6b36a0cc7d43f778fc4286c19a0
Mubashshir Khan <khan7@live.unc.edu>
2022-11-30 20:33:09 -0500

    fixed json and add server

---
+++ server.js
@@ -0,0 +1,7 @@
+import {roll} from "./lib/lib/roll.js";
+import minimist from "minimist";
+import express from "express";
+
+const args = minimist(process.argv.slice(2));
+const app = express();
+const port = args.port || 5000;
