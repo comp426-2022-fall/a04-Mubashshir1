5e1286d3c32d8c5b071008f70aa5ae3b1ad482ee
Mubashshir Khan <khan7@live.unc.edu>
2022-11-30 20:59:57 -0500

    added parsing to server

--- server.js
+++ server.js
@@ -5,3 +5,26 @@ import express from "express";
 const args = minimist(process.argv.slice(2));
 const app = express();
 const port = args.port || 5000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+app.get('/app/', (req, res) => {
+    res.send("200 OK");
+  })
+
+
+app.get('/app/roll', (req, res) => {
+    res.status(200).send(roll(6, 2, 1));
+})
+
+
+app.post('/app/roll', (req, res) => {
+    res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
+  })
+
+
+app.get('/app/roll/:sides/', (req, res) => {
+    res.send(roll(parseInt(req.params.sides),2, 1));
+  })
