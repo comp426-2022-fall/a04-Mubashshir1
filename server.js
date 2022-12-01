a718a0014b0e6ac1c1e3b1590ecbee70e7db8f30
Mubashshir Khan <khan7@live.unc.edu>
2022-11-30 21:07:36 -0500

    added functionality to server

--- server.js
+++ server.js
@@ -9,22 +9,27 @@ const port = args.port || 5000;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-
 app.get('/app/', (req, res) => {
     res.send("200 OK");
   })
 
-
 app.get('/app/roll', (req, res) => {
-    res.status(200).send(roll(6, 2, 1));
+    res.send(roll(6, 2, 1));
 })
-
 
 app.post('/app/roll', (req, res) => {
     res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
   })
 
-
 app.get('/app/roll/:sides/', (req, res) => {
     res.send(roll(parseInt(req.params.sides),2, 1));
   })
+
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
+})
+
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
+})
+
